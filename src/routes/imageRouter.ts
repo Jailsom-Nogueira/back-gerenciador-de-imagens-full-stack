419b4ae8f72fd01ff820b851af21f18efff058c6
Jailsom Nogueira <jailsom.nogueira@gmail.com>
2020-10-05 18:58:33 -0300

    criado projeto e endpoints de cadastro e login

---
+++ src/routes/imageRouter.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { ImageController } from '../controller/ImageController';
+
+export const imageRouter = express.Router();
+
+const imageController = new ImageController();
+
+imageRouter.post('/createImage', imageController.createImage);
+imageRouter.get('/getImage', imageController.getImage);
