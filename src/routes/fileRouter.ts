358bdb899ffb11f076b7f2235245a741383861bc
Jailsom Nogueira <jailsom.nogueira@gmail.com>
2020-10-13 18:26:59 -0300

    impl. componente para upload de arquivo e bibliotecas necessárias

---
+++ src/routes/fileRouter.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { FileController } from '../controller/FileController';
+
+export const fileRouter = express.Router();
+
+const fileController = new FileController();
+
+fileRouter.put('/upload', fileController.fileUpload);
