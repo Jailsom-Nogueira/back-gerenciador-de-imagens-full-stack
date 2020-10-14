07b363b4ee52c03e7feaaa01836484ba98b79a86
Jailsom Nogueira <jailsom.nogueira@gmail.com>
2020-10-14 10:31:50 -0300

    mudanças nos nomes das contantes S3

--- src/services/S3Service.ts
+++ src/services/S3Service.ts
@@ -3,8 +3,8 @@ import {S3} from 'aws-sdk';
 export class S3Service {
 
   private s3 = new S3({
-    accessKeyId: process.env.AWS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ,
   });
 
   public async uploadFile(input: UploadFileInput): Promise<UploadFileOutput>{
