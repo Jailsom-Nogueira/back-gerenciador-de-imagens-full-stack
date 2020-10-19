aa3d9eeff79ed6439a79bc1f7ab009aa46ae457e
Jailsom Nogueira <jailsom.nogueira@gmail.com>
2020-10-19 17:39:22 -0300

    retirada proxy cors

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,6 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use('/user', userRouter);
 app.use('/image', imageRouter);
 app.use('/files', fileRouter);
